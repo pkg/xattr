cb4e942660d4cf940d27a3cde98069d2f9d26a68
Jakob Unterwurzacher <jakobunt@gmail.com>
2018-05-23 20:13:03 +0200

    travis ci: drop cross-platform "go vet"
    Does not work properly before Go 1.10, see
    https://travis-ci.org/pkg/xattr/jobs/376069385#L466
    for an example:
    
    	Building for Darwin...
    	vet: import failed: can't find import: "fmt"
    	vet: import failed: can't find import: "fmt"
    	vet: import failed: can't find import: "net/http"
    	vet: import failed: can't find import: "net/http"
    	exit status 1
    
    Instead, run "go vet" for the native platform.

--- .travis.sh
+++ .travis.sh
@@ -2,15 +2,13 @@
 
 echo "Building for Linux..."
 GOOS=linux   go build
-GOOS=linux   go vet
 
 echo "Building for Darwin..."
 GOOS=darwin  go build
-GOOS=darwin  go vet
 
 echo "Building for FreeBSD..."
 GOOS=freebsd go build
-GOOS=freebsd go vet
 
 echo "Running tests..."
+go vet
 go test -v -race
